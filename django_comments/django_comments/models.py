dde2ce34957ac8471a47cfa3ad5cbe8539309c45
Puja0708 <singh.puja92@hotmail.cpm>
2015-08-22 22:48:15 +0530

    created models

---
+++ django_comments/django_comments/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Save_comments(models.Model):
+	user_id = models.CharField(max_length=250)
+	post_id = models.CharField(max_length=250)
+	comment = models.CharField(max_length=500)
+	
+	def __str__(self):
+		return self.title
+
+	class Admin:
+		pass
+
+
--- django_comments/django_comments/views.py
+++ django_comments/django_comments/views.py
@@ -10,4 +10,19 @@ def home(request):
 		return render_to_response('index.html')
 	elif request.method == 'POST':
 		print request.POST
+		comment = request.POST
+		comment.save()
 		return render_to_response('index.html')
+
+def comments_handler():
+
+    with open('comments.json', 'r') as file:
+        comments = json.loads(file.read())
+
+    if request.method == 'POST':
+        comments.append(request.form.to_dict())
+
+        with open('comments.json', 'w') as file:
+            file.write(json.dumps(comments, indent=4, separators=(',', ': ')))
+
+    return Response(json.dumps(comments), mimetype='application/json', headers={'Cache-Control': 'no-cache'})
