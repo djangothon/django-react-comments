92bb1608e3aab3209e94d38d47e1e09cf9edb854
Kumar Anirudha <anirudhastark@yahoo.com>
2015-08-22 20:21:04 +0530

    csrf token exempt

--- django_comments/django_comments/views.py
+++ django_comments/django_comments/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
+@csrf_exempt
 def home(request):
 	context_instance=RequestContext(request)
 	print request
