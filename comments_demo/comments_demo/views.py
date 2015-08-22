80305ea36a7f9082af9a340d09b80a58a65b8321
Kumar Anirudha <anirudhastark@yahoo.com>
2015-08-22 19:54:30 +0530

    structuring. bug found. csrf token error.

--- comments_demo/comments_demo/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, render_to_response
-
-def home(request):
-	print request
-	if request.method == 'POST':
-		print 'in'
-		return render_to_response('demo.html')
-	else:
-		return render_to_response('demo.html')
